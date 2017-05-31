09e5e559fe8ed1c6dae2f85ac01e5975e84905dd
Oskar <rakso97@o2.pl>
2017-06-01 00:38:59 +0200

    Added CarTypeRepository.cs with Interfaces/ICarTypeRepository.cs

---
+++ RentalCar/RentalCar.DataLayer/Services/CarTypeRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentalCar.DataLayer.Models;
+using RentalCar.DataLayer.Services.Basic;
+using RentalCar.DataLayer.Services.Interfaces;
+
+namespace RentalCar.DataLayer.Services
+{
+    /// <summary>
+    /// Klasa obługująca model CarType w relacji z bazą
+    /// </summary>
+    public class CarTypeRepository : BasicRepository<CarType>, ICarTypeRepository
+    {
+        /// <summary>
+        /// Dodawanie modelu CarType do bazy danych
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public override bool Add(CarType model)
+        {
+            return ExecuteQuery(dbContext =>
+            {
+                dbContext.CarTypesDbSet.Add(model);
+                return true;
+            });
+        }
+
+        /// <summary>
+        /// Pobieranie z bazy modelu CarType po id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public override CarType Get(int id)
+        {
+            return ExecuteQuery(dbContext => dbContext.CarTypesDbSet.First(car => car.Id == id));
+        }
+
+        /// <summary>
+        /// Zwracanie wszystki elementów CarType z tabeli
+        /// </summary>
+        /// <returns></returns>
+        public override List<CarType> GetAll()
+        {
+            return ExecuteQuery(dbContext => dbContext.CarTypesDbSet.ToList());
+        }
+    }
+}
---
+++ RentalCar/RentalCar.DataLayer/Services/Interfaces/ICarTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RentalCar.DataLayer.Models;
+
+namespace RentalCar.DataLayer.Services.Interfaces
+{
+    public interface ICarTypeRepository
+    {
+        bool Add(CarType model);
+        CarType Get(int id);
+        List<CarType> GetAll();
+    }
+}
