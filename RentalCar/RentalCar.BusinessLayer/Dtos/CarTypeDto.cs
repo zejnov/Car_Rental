b1b09be775b754baa11387e0730b2359bf5f3466
Mati ZejnoV <mati.szwaba@gmail.com>
2017-05-31 19:16:26 +0200

    Add CarTypeDto.cs Remove Program.cs in BusinessLayer

---
+++ RentalCar/RentalCar.BusinessLayer/Dtos/CarTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar.BusinessLayer.Dtos
+{
+    class CarTypeDto
+    {
+        public string Mark;
+        public string Model;
+        public int PricePerDay;
+    }
+}
