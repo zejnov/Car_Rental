1ca287edc2c6d54f476aef64d6c410b8c4ebd5f6
Mati ZejnoV <mati.szwaba@gmail.com>
2017-05-31 18:49:03 +0200

    Add Layers References Add Models/CarType.cs

---
+++ RentalCar/RentalCar.DataLayer/Models/CarType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar.DataLayer.Models
+{
+    class CarType
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public int PricePerDay { get; set; }
+    }
+}
