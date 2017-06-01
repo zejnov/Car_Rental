e5d907f40dee89bc2382ee6aab484c204d8b0ba7
Oskar <rakso97@o2.pl>
2017-06-01 17:38:54 +0200

    Add command functionality

---
+++ RentalCar/RentalCar.Cli/Commands/CommandAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCar.Cli.Commands
+{
+    class CommandAction
+    {
+        public string Command { get; private set; }
+        public string Description { get; private set; }
+
+        private Func<bool> _action;
+
+        public CommandAction (string command, string description, Func<bool> action)
+        {
+            Command = command;
+            Description = description;
+            _action = action;
+        }
+
+        public bool ExecuteCommandAction()
+        {
+            return _action();
+        }
+    }
+}
---
+++ RentalCar/RentalCar.Cli/Commands/CommandDispatcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalCar.Cli.Commands
+{
+    class CommandDispatcher
+    {
+        private List<CommandAction> _validCommands = new List<CommandAction>();
+
+        public bool AddCommand(string command, string description, Func<bool> action)
+        {
+            if (ConstainsCommand(command))
+                return false;
+
+            _validCommands.Add(new CommandAction(command, description, action));
+            return true;
+        }
+
+        public bool ConstainsCommand(string command)
+        {
+            return (FindCommandAciotn(command) != null);
+        }
+
+        public bool Dispatch(string command)
+        {
+            CommandAction commandAction = FindCommandAciotn(command);
+
+            if (commandAction != null)
+                return commandAction.ExecuteCommandAction();
+
+            return false;
+        }
+
+        public List<CommandAction> GetValidCommandActions()
+        {
+            return _validCommands;
+        }
+
+        private CommandAction FindCommandAciotn(string command)
+        {
+            return _validCommands.Find(p => p.Command == command);
+        }
+    }
+}
