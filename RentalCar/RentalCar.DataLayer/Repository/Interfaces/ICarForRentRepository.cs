05bf9f8b3fecbeb142109d051160e69d7751989e
Oskar <rakso97@o2.pl>
2017-06-01 20:02:23 +0200

    Added CarForRent Repository

---
+++ RentalCar/RentalCar.DataLayer/Repository/Interfaces/ICarForRentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using RentalCar.DataLayer.Models;
+
+namespace RentalCar.DataLayer.Repository.Interfaces
+{
+    public interface ICarForRentRepository
+    {
+        bool Add(CarForRent model);
+        bool Exist(CarForRent model);
+        CarForRent Get(int id);
+        List<CarForRent> GetAll();
+    }
+}
