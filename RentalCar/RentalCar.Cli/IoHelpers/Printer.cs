03578698cca313913610de7438991d05f9d7f2fe
Oskar <rakso97@o2.pl>
2017-06-01 20:11:03 +0200

    Add choosing CarType from list

--- RentalCar/RentalCar.Cli/IoHelpers/Printer.cs
+++ RentalCar/RentalCar.Cli/IoHelpers/Printer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RentalCar.BusinessLayer.Dtos;
 using RentalCar.Cli.Commands;
 
 namespace RentalCar.Cli.IoHelpers
@@ -12,6 +13,11 @@ namespace RentalCar.Cli.IoHelpers
         public static void PrintObjectData(CommandAction command)
         {
             Console.WriteLine($"{command.Command} - {command.Description}");
+        }
+
+        public static void PrintOrderedList(CarTypeDto courseMember, int ordinal)
+        {
+            Console.WriteLine($"{ordinal}. {courseMember.Mark} {courseMember.Model} {courseMember.PricePerDay}zl/day");
         }
     }
 }
