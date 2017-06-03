1e6dd82094afa59bd1836cb14556fe5db4fcb679
Oskar <rakso97@o2.pl>
2017-06-03 10:34:31 +0200

    Added Calculate price width test

---
+++ RentalCar/RentalCar.BusinessLayer.Tests/CarRentedByCustomerServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentalCar.BusinessLayer.Dtos;
+using RentalCar.BusinessLayer.Services;
+
+namespace RentalCar.BusinessLayer.Tests
+{
+    [TestClass]
+    public class CarRentedByCustomerServiceTests
+    {
+        [TestMethod]
+        public void GetPrice_ValidCarRentedByCustomerWidthoutRabat_PriceDoubleReturned()
+        {
+            var inputRented = new CarsRentedByCustomersDto()
+            {
+                Customer = new CustomerDto()
+                {
+                  Id  = 1,
+                  Name = "Test",
+                  Pesel = 12345678901,
+                  Surname = "Test",                 
+                },
+
+                Id = 10,
+                CarForRental = new CarForRentDto()
+                {
+                    TypeOfCar = new CarTypeDto()
+                    {
+                        Mark = "Test",
+                        PricePerDay = 200,
+                        Model = "Test",
+                        Id = 5
+                    },
+                    Id = 20,
+                    IsRented = true,
+                    RegistrationNumber = "Test"
+                },
+                IsReturned = false,
+                RentalDateTime = new DateTime(2017,06,01),
+            };
+
+            inputRented.Customer.CarsRentedByCustomersList.Add(inputRented);
+            inputRented.CarForRental.CarsRentedByCustomersList.Add(inputRented);
+
+            var price = CarRentedByCustomerDtoServices.GetPrice(inputRented);
+
+            Assert.AreEqual(400D, price);
+        }
+
+        [TestMethod]
+        public void GetPrice_ValidCarRentedByCustomerWidthRabat_PriceDoubleReturned()
+        {
+            var inputRented = new CarsRentedByCustomersDto()
+            {
+                Customer = new CustomerDto()
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Pesel = 12345678901,
+                    Surname = "Test",
+                },
+
+                Id = 10,
+                CarForRental = new CarForRentDto()
+                {
+                    TypeOfCar = new CarTypeDto()
+                    {
+                        Mark = "Test",
+                        PricePerDay = 200,
+                        Model = "Test",
+                        Id = 5
+                    },
+                    Id = 20,
+                    IsRented = true,
+                    RegistrationNumber = "Test"
+                },
+                IsReturned = false,
+                RentalDateTime = new DateTime(2017, 06, 01),
+            };
+
+            for (int i = 0; i < 11; i++)
+            {
+                inputRented.Customer.CarsRentedByCustomersList.Add(inputRented);
+            }
+
+            inputRented.CarForRental.CarsRentedByCustomersList.Add(inputRented);
+
+            var price = CarRentedByCustomerDtoServices.GetPrice(inputRented);
+
+            Assert.AreEqual(380D, price);
+        }
+    }
+}
