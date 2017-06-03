6ff8f5a7e5b466af79d6ffb9f7a5557fde5d4550
Oskar <rakso97@o2.pl>
2017-06-03 12:46:35 +0200

    Add Serivice and Repository for Sale

---
+++ RentalCar/RentalCar.BusinessLayer/Services/SaleDtoServices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentalCar.BusinessLayer.Dtos;
+using RentalCar.BusinessLayer.Mappers;
+using RentalCar.DataLayer.Models;
+using RentalCar.DataLayer.Repository;
+
+namespace RentalCar.BusinessLayer.Services
+{
+    public class SaleDtoServices
+    {
+        // <summary>
+        /// Dodaje nowy, unikatowy sale, zwraca false jeżeli taki już jest
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static bool Add(SaleDto saleDto)
+        {
+            if (Exist(saleDto))
+                return false;
+
+            return new SaleRepository().Add(DtoToEntityMapper.SaleEntityModelToDto(saleDto));
+        }
+
+        /// <summary>
+        /// Sprawdza czy dany Dto istniej jako model w bazie danych
+        /// </summary>
+        /// <param name="saleDto"></param>
+        /// <returns></returns>
+        public static bool Exist(SaleDto saleDto)
+        {
+            return new SaleRepository()
+                .Exist(DtoToEntityMapper.SaleEntityModelToDto(saleDto));
+        }
+
+        /// <summary>
+        /// Zwraca wszystkie modele jako lista Dto z bazy danych
+        /// </summary>
+        /// <returns></returns>
+        public static List<SaleDto> GetAll()
+        {
+            return new SaleRepository()
+                .GetAll()
+                .Select(EntityToDtoMapper.SaleEntityModelToDto)
+                .ToList();
+        }
+    }
+}
+}
